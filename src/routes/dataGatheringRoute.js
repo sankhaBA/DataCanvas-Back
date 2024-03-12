f690136f93022bbc89a236bfbc3a3236b08b9b66
imnthiyanga <ithiyanga@gmail.com>
2024-03-12 20:52:12 +0530

    Fixing AB#93 Routes for indexing column and value

--- src/routes/dataGatheringRoute.js
+++ src/routes/dataGatheringRoute.js
@@ -18,6 +18,22 @@ router.post('/insert', (req, res) => {
     }
 });
 
+// put request to update data
+router.put('/update', (req, res) => {
+    const { project_id, fingerprint, table } = req.body;
+
+    try {
+        if (validateFields(req.body) && validateDataFields(req.body)) {  // if project_id, fingerprint and table_id parameters are available, update data
+            DataGatheringController.updateData(req, res);
+        } else {  // If project_id, fingerprint and table_id parameters are not available, send bad request
+            res.status(400).json({ error: 'Bad Request | CHECK project_id, fingerprint, table, data | id and device fields are automatically added | Request validation unsuccessful' });
+        }
+    } catch (error) {
+        console.error('Error updating data:', error);
+        res.status(500).json({ message: 'Failed to update data' });
+    }
+});
+
 const validateFields = (fields) => {
     const { project_id, fingerprint, table, data } = fields;
 
@@ -53,6 +69,16 @@ const validateFields = (fields) => {
 
     // Check if the data section has 'id' or 'device' section. If so, return false
     if (data.id || data.device) {
+        return false;
+    }
+
+    return true;
+}
+
+//validate update fields data
+const validateDataFields = (data) => {
+    if(!data.id || !data.value){
+        console.log('index_id and index_value are required');
         return false;
     }
 
