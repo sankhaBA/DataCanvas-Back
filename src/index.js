90c41bdb76cd4c13256f5ac3cef9b5cd2dc89b53
Sankha Bimsara <sankha.b21@gmail.com>
2024-03-25 15:16:47 +0530

    JWT restored

--- src/index.js
+++ src/index.js
@@ -18,14 +18,14 @@ const dataRetrievalRoute = require("./routes/dataRetrievalRoute");
 const PORT = process.env.PORT || 3001;
 
 app.use("/api/user", userRoute);
-app.use("/api/auth", authRoute);
-app.use("/api/project", projectRoute);
-app.use("/api/device", deviceRoute);
-app.use("/api/data/tbl", dataTableHandlingRoute);
-app.use("/api/data/clm", columnRoute);
-app.use("/api/data/config", dataConfigRoute);
-app.use("/api/data/feed", dataGatheringRoute);
-app.use("/api/data/get", dataRetrievalRoute);
+app.use("/api/auth", verifyToken, authRoute);
+app.use("/api/project", verifyToken, projectRoute);
+app.use("/api/device", verifyToken, deviceRoute);
+app.use("/api/data/tbl", verifyToken, dataTableHandlingRoute);
+app.use("/api/data/clm", verifyToken, columnRoute);
+app.use("/api/data/config", verifyToken, dataConfigRoute);
+app.use("/api/data/feed", dataGatheringRoute); // JWT middleware is not needed because data sent through this route is public
+app.use("/api/data/get", verifyToken, dataRetrievalRoute);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
